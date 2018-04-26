8675634f41e42797a68a4563bf56a4d7838a692d
GARTNER\sbansal <saransh.bansal@gartner.com>
2018-04-26 15:50:08 +0530

    no message

--- src/modules/Menu/menuItem.js
+++ src/modules/Menu/menuItem.js
@@ -2,6 +2,9 @@ import React from 'react';
 import StarRatingComponent from 'react-star-rating-component';
 
 const MenuItem = (props) => {
+    const op = Number(props.data.price);
+    const d = Number(props.data.discount);
+    let finalPrice = op - (Number(op) * Number(d) / 100);
     return (
         <div className="col-xs-12 col-md-12 card-container">
             <div className="card">
@@ -14,7 +17,15 @@ const MenuItem = (props) => {
                         value={props.data.rating}
                     />
                     <br/>
-                    <span className="card-price">{'Price - ₹' + props.data.price}</span>
+                    {props.data.discount > 0 ? (
+                        <div>
+                            {'Price - '}<span className="card-price original-price">{'₹' + props.data.price}</span>
+                            &nbsp;&nbsp;&nbsp;
+                            <span className="card-price discounted-price">{'₹' + finalPrice + ' (-'+ props.data.discount+'%)'}</span>
+                        </div>
+                    ) : (
+                        <span className="card-price">{'Price - ₹' + props.data.price}</span>
+                    )}
                     <hr />
                     <span className="ingredients">{'Ingredients: ' + props.data.ingredients.toString()}</span>
                 </div>
